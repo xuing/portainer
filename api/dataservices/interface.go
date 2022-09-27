0dcd5c3bfdb6ef67dd64280fa7b906ef50b93cc9
Matt Hook <hookenz@gmail.com>
2022-09-27 16:30:14 +1300

    minor changes to the version interface

--- api/dataservices/interface.go
+++ api/dataservices/interface.go
@@ -308,7 +308,7 @@ type (
 		UpdateInstanceID(ID string) error
 		Version() (*models.Version, error)
 		UpdateVersion(*models.Version) error
-		Migrate() (bool, error)
+		Migrate() error
 		BucketName() string
 	}
 
